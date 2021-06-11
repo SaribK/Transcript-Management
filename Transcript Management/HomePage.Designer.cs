05daf1b05f17b2877307cdd5e643fb9fc5995a0d
SaribK <sarib.kashif.2001@gmail.com>
2021-06-11 03:27:59 -0400

    displays overall gpa and gpa for specific year

--- Transcript Management/HomePage.Designer.cs
+++ Transcript Management/HomePage.Designer.cs
@@ -34,7 +34,6 @@ namespace Transcript_Management
             this.label1 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -63,7 +62,7 @@ namespace Transcript_Management
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(731, 27);
+            this.label1.Location = new System.Drawing.Point(703, 27);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(129, 20);
             this.label1.TabIndex = 2;
@@ -78,37 +77,27 @@ namespace Transcript_Management
             "2",
             "3",
             "4"});
-            this.comboBox1.Location = new System.Drawing.Point(878, 61);
+            this.comboBox1.Location = new System.Drawing.Point(706, 61);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(40, 21);
             this.comboBox1.TabIndex = 3;
+            this.comboBox1.TextChanged += new System.EventHandler(this.comboBox1_TextChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(731, 62);
+            this.label2.Location = new System.Drawing.Point(752, 62);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(141, 20);
+            this.label2.Size = new System.Drawing.Size(149, 20);
             this.label2.TabIndex = 4;
-            this.label2.Text = "Specific Year GPA";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(924, 62);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(30, 20);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "xxx";
+            this.label2.Text = "Specific Year GPA: ";
             // 
             // formHome
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(972, 531);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label1);
@@ -116,6 +105,7 @@ namespace Transcript_Management
             this.Controls.Add(this.button1);
             this.Name = "formHome";
             this.Text = "Home Page";
+            this.Load += new System.EventHandler(this.formHome_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -128,6 +118,5 @@ namespace Transcript_Management
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
     }
 }
--- Transcript Management/HomePage.cs
+++ Transcript Management/HomePage.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -17,6 +18,8 @@ namespace Transcript_Management
             InitializeComponent();
         }
 
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-OFIG42E\\SQLEXPRESS;Initial Catalog=TranscriptDB;Persist Security Info=True;User ID=sarib;Password=sarib2001");
+
         private void button1_Click(object sender, EventArgs e)
         {
             formInsert fI = new formInsert();
@@ -29,6 +32,35 @@ namespace Transcript_Management
             formSearch fS = new formSearch();
             this.Hide();
             fS.ShowDialog();
+        }
+
+        private void formHome_Load(object sender, EventArgs e)
+        {
+            SqlCommand command = new SqlCommand("SELECT AVG(courseGrade) as GPA from grades_table", con);
+            SqlDataAdapter sd = new SqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            sd.SelectCommand.CommandType = CommandType.Text;
+            sd.Fill(dt);
+            string value = dt.Rows[0].ItemArray[0].ToString();
+            label1.Text = "Overall GPA: " + value;
+        }
+
+        private void comboBox1_TextChanged(object sender, EventArgs e)
+        {
+            SqlCommand command2 = new SqlCommand("SELECT AVG(courseGrade) as GPA from grades_table where courseYear = '" + int.Parse(comboBox1.Text) + "'", con);
+            SqlDataAdapter sd2 = new SqlDataAdapter(command2);
+            DataTable dt2 = new DataTable();
+            sd2.SelectCommand.CommandType = CommandType.Text;
+            sd2.Fill(dt2);
+            string value2 = dt2.Rows[0].ItemArray[0].ToString();
+            if (value2 == "")
+            {
+                label2.Text = "Specific Year GPA: N/A";
+            }
+            else
+            {
+                label2.Text = "Specific Year GPA: " + value2;
+            }
         }
     }
 }
