dad96c27f1b812e983419ff157f6bd69674b65d7
SaribK <kashifsar01@gmail.com>
2021-06-11 16:14:46 -0400

    checking if commit shows up on heatmap

--- Transcript Management/InsertForm.Designer.cs
+++ Transcript Management/InsertForm.Designer.cs
@@ -183,7 +183,7 @@ namespace Transcript_Management
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.White;
-            this.label5.Location = new System.Drawing.Point(378, 25);
+            this.label5.Location = new System.Drawing.Point(379, 25);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(177, 36);
             this.label5.TabIndex = 7;
